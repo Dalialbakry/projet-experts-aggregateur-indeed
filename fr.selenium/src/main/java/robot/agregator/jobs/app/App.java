b2747cfe124b997724203edd4e6846002a4dd355
Sabri <sabri.taleb@outlook.com>
2019-06-17 16:51:03 +0200

    ajout de l'envoi de mail

--- fr.selenium/src/main/java/robot/agregator/jobs/app/App.java
+++ fr.selenium/src/main/java/robot/agregator/jobs/app/App.java
@@ -1,14 +1,29 @@
 package robot.agregator.jobs.app;
 
-import robot.agregator.jobs.loaders.AppFlow;
+import robot.agregator.jobs.constants.JobsWebSites;
 import robot.agregator.jobs.loaders.MyDriver;
+import robot.agregator.jobs.mailing.Mailing;
 
-public class App extends AppFlow{
+public class App {
 
+	public static void App1() {
+		new MyDriver();
+		MyDriver.driver.get(JobsWebSites.INDEED_HOME_PAGE);
+	}
+	
 	public static void main(String[] args) {
 
-		run();
+		App1();
+		Mailing.sendMessage("Bonjour", "coucou", "sabri.taleb@outlook.com", "expertprojet2019@gmail.com");
 		MyDriver.quitDriver();
 	}
 
+	
+	private static void pause(int seconds) {
+		try {
+			Thread.sleep(seconds * 1000);
+		} catch (InterruptedException ex) {
+			Thread.currentThread().interrupt();
+		}
+	}
 }
