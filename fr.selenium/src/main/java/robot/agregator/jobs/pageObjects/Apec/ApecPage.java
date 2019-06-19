99de8383339cb2afbbe55c6f1fa850754a27ada0
Dalia Elbakrygit config --global user.email you@example.com <dalia@elbakry.fr>
2019-06-19 17:07:42 +0200

    apec package done

--- fr.selenium/src/main/java/robot/agregator/jobs/pageObjects/Apec/ApecPage.java
+++ fr.selenium/src/main/java/robot/agregator/jobs/pageObjects/Apec/ApecPage.java
@@ -1,5 +1,37 @@
 package robot.agregator.jobs.pageObjects.Apec;
 
-public class ApecPage {
+import static org.testng.Assert.assertEquals;
+
+import robot.agregator.jobs.pageObjects.AllPages;
+
+public class ApecPage extends AllPages {
+
+	private static final String APEC_PAGE = "";
+
+	// open apec page
+	public ApecPage() {
+		super();
+		System.out.println("Welcome to APEC website for job search");
+	}
+
+	public void run() {
+
+		performanceActions();
+		performanceChecks();
+	}
+
+	private void performanceActions() {
+		ApecPageActions actions = new ApecPageActions();
+		actions.setPoste();
+		actions.setVille();
+		actions.clickSearch();
+
+	}
+
+	public void performanceChecks() {
+		ApecPageChecks check = new ApecPageChecks();
+		assertEquals();
+		System.out.println("if wrong we don't see it");
+	}
 
 }
