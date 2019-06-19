056d96703e155e896d62f0331571fa65cb3bbc9d
Sabri <sabri.taleb@outlook.com>
2019-06-19 16:54:26 +0200

    Ajout de la liste offres indeed

--- fr.selenium/src/main/java/robot/agregator/jobs/app/App.java
+++ fr.selenium/src/main/java/robot/agregator/jobs/app/App.java
@@ -1,31 +1,45 @@
 package robot.agregator.jobs.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.By.ById;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+
 import robot.agregator.jobs.constants.JobsWebSites;
+import robot.agregator.jobs.listeJobs.Indeed_JobList;
 import robot.agregator.jobs.loaders.MyDriver;
 import robot.agregator.jobs.mailing.ContenuMail;
 import robot.agregator.jobs.mailing.Mailing;
+import robot.agregator.jobs.pageObjects.Apec.ApecPage;
+import robot.agregator.jobs.pageObjects.Indeed.IndeedPage;
+import robot.agregator.jobs.pageObjects.Indeed.Indeed_IDs;
 
 // classe permettant de lancer le robot
 public class App {
 
-	public static void App1() {
-		new MyDriver();
-		MyDriver.driver.get(JobsWebSites.INDEED_HOME_PAGE);
-	}
 
+	
 	public static void main(String[] args) {
 
 		// action qui lance la connexion au browser
-		App1();
-
+		IndeedPage.App1();
+		IndeedPage.IndeedSearch();
+		pause(3);
+		Indeed_JobList.recupererOffres();
 		// action pour l'envoi de mail groupé
-		Mailing.sendMessage(ContenuMail.SUBJECT, ContenuMail.TEXT, ContenuMail.DESTINATAIRES, ContenuMail.COPYDEST);
+		//Mailing.sendMessage(ContenuMail.SUBJECT, ContenuMail.TEXT, ContenuMail.DESTINATAIRES, ContenuMail.COPYDEST);
 		
 		// action pour l'envoi du mail simple
 		// Mailing.sendMessage("Bonjour", "coucou", "sabri.taleb@outlook.com","expertprojet2019@gmail.com");
 		
 		// action pour fermer la page
 		MyDriver.quitDriver();
+		pause(5);
+		ApecPage.App1();
+		ApecPage.ApecSearch();
 	}
 
 	private static void pause(int seconds) {
--- fr.selenium/src/main/java/robot/agregator/jobs/pageObjects/Apec/ApecPage.java
+++ fr.selenium/src/main/java/robot/agregator/jobs/pageObjects/Apec/ApecPage.java
@@ -1,5 +1,26 @@
 package robot.agregator.jobs.pageObjects.Apec;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
+import robot.agregator.jobs.constants.JobsWebSites;
+import robot.agregator.jobs.loaders.MyDriver;
+import robot.agregator.jobs.pageObjects.Indeed.Indeed_IDs;
+
 public class ApecPage {
+	public static void App1() {
+		new MyDriver();
+		MyDriver.driver.get(JobsWebSites.APEC_HOME_PAGE);
+	}
+	
+	
+	
+	public static void ApecSearch() {
+		MyDriver.driver.findElement(By.id(Indeed_IDs.ID_SearchJob)).sendKeys("testeur logiciel");
+		MyDriver.driver.findElement(By.id(Indeed_IDs.ID_SearchWhere)).sendKeys((Keys.chord(Keys.CONTROL, "a")));
+		MyDriver.driver.findElement(By.id(Indeed_IDs.ID_SearchWhere)).sendKeys("Alpes Maritimes");
+		MyDriver.driver.findElement(By.id(Indeed_IDs.ID_SearchWhere)).sendKeys(Keys.ENTER);
+		//MyDriver.driver.findElement(By.className("icl-WhatWhere-buttonWrapper")).click();
+	}
 
 }
