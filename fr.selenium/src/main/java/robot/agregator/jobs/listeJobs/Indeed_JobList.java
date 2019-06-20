c1f4fb56d29c21265514c22aa538483023a35d1b
Sabri <sabri.taleb@outlook.com>
2019-06-20 10:33:46 +0200

    Finalisation liste offres Indeed

--- fr.selenium/src/main/java/robot/agregator/jobs/listeJobs/Indeed_JobList.java
+++ fr.selenium/src/main/java/robot/agregator/jobs/listeJobs/Indeed_JobList.java
@@ -9,27 +9,27 @@ import org.openqa.selenium.WebElement;
 import robot.agregator.jobs.loaders.MyDriver;
 
 public class Indeed_JobList {
+	public static ArrayList<Offre> listeOffres = new ArrayList<>();
 
-public static void recupererOffres() {
-		
-		ArrayList<String> listeEmplois = new ArrayList<String>();
-		
-		for (int i=0; i<3; i++) {
-		List<WebElement> jobs = MyDriver.driver.findElements(By.cssSelector(".jobtitle.turnstileLink"));
-		List<WebElement> jobSummary = MyDriver.driver.findElements(By.cssSelector(".summary"));
-		List<WebElement> jobLocation = MyDriver.driver.findElements(By.cssSelector(".location"));
-		String link = jobs.get(i).getAttribute("href");
-		String title = jobs.get(i).getAttribute("title");
-		String summary = jobSummary.get(i).getText();
-		String location = jobLocation.get(i).getText();
-		
-		
-		listeEmplois.add(link);
-		listeEmplois.add(title);
-		listeEmplois.add(summary);
-		listeEmplois.add(location);
+	public static void recupererOffres() {
+
+		for (int i = 0; i < 3; i++) {
+			List<WebElement> jobs = MyDriver.driver.findElements(By.cssSelector(".jobtitle.turnstileLink"));
+			List<WebElement> jobSummary = MyDriver.driver.findElements(By.cssSelector(".summary"));
+			List<WebElement> jobLocation = MyDriver.driver.findElements(By.cssSelector(".location"));
+			String link = jobs.get(i).getAttribute("href");
+			String title = jobs.get(i).getAttribute("title");
+			String summary = jobSummary.get(i).getText();
+			String location = jobLocation.get(i).getText();
+
+			Offre offre = new Offre(title, link, summary, location);
+
+			listeOffres.add(offre);
+
 		}
-		
-		System.out.println(listeEmplois);
+		for (Offre offre : listeOffres) {
+
+			System.out.println(offre);
+		}
 	}
 }
